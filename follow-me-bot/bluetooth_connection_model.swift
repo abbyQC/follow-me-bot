7023665b183599cc06ee7b6e1e861e65108644fe
Chang Qiu <qcabby1217@gmail.com>
2020-02-10 01:17:32 -0500

    Initial Commit

---
+++ follow-me-bot/bluetooth_connection_model.swift
@@ -0,0 +1,9 @@
+//
+//  bluetooth_connection_model.swift
+//  follow-me-bot
+//
+//  Created by Chang Qiu on 2020-02-09.
+//  Copyright © 2020 Chang Qiu. All rights reserved.
+//
+
+import Foundation
---
+++ follow-me-bot/view controller/mainViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  follow-me-bot
+//
+//  Created by Chang Qiu on 2020-02-03.
+//  Copyright © 2020 Chang Qiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var button_pressed = true
+
+    @IBAction func start(_ sender: UIButton)
+    {
+        //button_pressed = !button_pressed
+        //
+         //updateUI()
+    }
+    
+    @IBAction func unwindToRed (unwindSegue: UIStoryboardSegue){
+        
+    }
+    //func updateUI(){
+     //    if button_pressed{
+     //       view.backgroundColor = .white
+            
+     //    }else{
+    //      view.backgroundColor = .black
+       
+            
+        //}
+        
+   
+    
+   // }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
